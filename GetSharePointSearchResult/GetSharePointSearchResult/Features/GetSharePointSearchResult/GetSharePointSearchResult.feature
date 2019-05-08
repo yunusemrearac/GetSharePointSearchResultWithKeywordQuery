68a557d35dce4a98f48c7c1a39088fd374fe2ae6
Yunus Emre Araç <earac@nuevo.com.tr>
2019-05-08 18:19:53 +0300

    .

---
+++ GetSharePointSearchResult/GetSharePointSearchResult/Features/GetSharePointSearchResult/GetSharePointSearchResult.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="783306dc-c473-4066-b44a-6f4aff661099" featureId="783306dc-c473-4066-b44a-6f4aff661099" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="GetSharePointSearchResult" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="527065c1-0479-47f7-ad0a-e02b614a45ff" />
+  </projectItems>
+</feature>
---
+++ GetSharePointSearchResult/GetSharePointSearchResult/GetSharePointSearchResult/GetSharePointSearchResultUserControl.ascx.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.SharePoint;
+using Microsoft.SharePoint.Search.Query;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web.UI;
+
+namespace GetSharePointSearchResult.GetSharePointSearchResult
+{
+    public partial class GetSharePointSearchResultUserControl : UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            using (SPSite site = SPContext.Current.Site)
+            {
+                KeywordQuery kq = new KeywordQuery(site);
+                kq.SourceId = new Guid("8413cd39-2156-4e00-b54d-11efd9abdb89");
+                kq.QueryText = "* " + searchText.Text;
+                kq.SelectProperties.Add("Title");
+                kq.SelectProperties.Add("Path");
+                kq.RowLimit = 20;
+                ResultTableCollection resultTables = new SearchExecutor().ExecuteQuery(kq);
+
+                DataTable resultDataTable = resultTables.FirstOrDefault().Table;
+
+                List<SearchResult> searchResultList = new List<SearchResult>();
+
+                foreach (DataRow row in resultDataTable.Rows)
+                {
+                    SearchResult searchResult = new SearchResult();
+
+                    searchResult.Title = row["Title"].ToString();
+                    searchResult.Url = row["Path"].ToString();
+
+                    searchResultList.Add(searchResult);
+                }
+
+                rptSearch.DataSource = searchResultList;
+                rptSearch.DataBind();
+            }
+        }
+
+        public class SearchResult
+        {
+            public string Title { set; get; }
+            public string Url { set; get; }
+        }
+    }
+}
